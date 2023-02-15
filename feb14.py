3a4af43fdb52a77c26f7e2e5f2b4103089378a61
Marty De Alba <103968668+martydealba27@users.noreply.github.com>
2023-02-15 13:06:12 -0800

    Add files via upload

---
+++ feb14.py
@@ -0,0 +1,18 @@
+print("    /I")
+print("   / I")
+print("  /  I")
+print(" /   I")
+print("/____I")
+#running a module is just executing instructions
+#python will do this in the order that the code is instructed
+
+character_name = "Vic"
+character_age = "100"
+print("there was once a man named " + character_name + ", ")
+print(character_name + " was " + character_age + " years old.")
+
+character_name = "Grant"
+
+print("He really like the name " + character_name + ", ")
+print("but he did not like being " + character_age + ".")
+
