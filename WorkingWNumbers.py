20a4b6a7502397d7bad0c64fb9e7fd9ef5f33520
Marty De Alba <103968668+martydealba27@users.noreply.github.com>
2023-02-16 14:20:07 -0800

    Add files via upload

---
+++ WorkingWNumbers.py
@@ -0,0 +1,29 @@
+from math import *
+
+#python
+
+print(69)
+print(7/34)
+
+# % modulus - run division and the answer is the remainder
+#10 dividided by 3 is remainder 1.
+#10%3= 1 
+
+print(10%3)
+
+#if you want to print a number and a string, make sure you
+#concern the number to a string 
+num = -5
+x = 34
+print(x-num)
+print(str(x-num) + " my favorite number") 
+
+#consider functions in relation to numbers
+
+print(abs(num))
+
+print(max(45, 23, 26, 27, 10))
+print(min(45, 23, 26, 27, 10))
+
+#round = round
+
