3d23776b1852f11fc60d6c434ad3b4e9073f2407
Marty De Alba <103968668+martydealba27@users.noreply.github.com>
2023-02-20 14:13:23 -0800

    Add files via upload

---
+++ InputFromUsers.py
@@ -0,0 +1,13 @@
+#name = input("Enter your name:")
+#age = input("Enter your age:")
+#print("Hello " + name + "!" "You are " + age + ".")
+
+num1 = input("Enter a number:")
+num2 = input("Enter another number: ")
+
+#result = num1 + num2
+#print(result)
+
+result = float(num1) + float(num2)
+print(result)
+
