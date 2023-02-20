4609c128d50ffa4c515da7c85c0a2bbf4659b43c
Marty De Alba <103968668+martydealba27@users.noreply.github.com>
2023-02-20 14:13:47 -0800

    Add files via upload

---
+++ MadLibs.py
@@ -0,0 +1,8 @@
+color = input("Enter a color:")
+plural_noun = input("Enter a plural noun:")
+celeb = input("Enter a celebrity:")
+
+print("roses are " + color)
+print(plural_noun + " are blue.")
+print("I love " + celeb)
+
