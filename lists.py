931dc8d1c3362d15ade2c56a7bb561fba53d866b
Marty De Alba <103968668+martydealba27@users.noreply.github.com>
2023-02-20 15:07:08 -0800

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,21 @@
+friends = ["Marty", "Karen", "Jimbo", "Butters", "Timbo", "Toby",]
+lucky_numbers = [4, 27, 91, 54, 23, 76]
+
+friends.append("Bruce")
+
+friends.remove("Marty")
+#indexing can start from the back if you use negative numbers
+
+print(friends)
+
+friends.sort()
+print(friends)
+
+print(lucky_numbers)
+
+lucky_numbers.sort()
+print(lucky_numbers)
+
+friends2 = friends.copy()
+
+print(friends2)
