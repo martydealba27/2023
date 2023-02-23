c37d31ef5aa8f6feb37a3fc07ccdff96321b1bbe
Marty De Alba <103968668+martydealba27@users.noreply.github.com>
2023-02-22 21:20:36 -0800

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,10 @@
+#functions help you organize code 
+#core concept in pything programming 
+
+#creating a function that will say hi 
+
+def sayhi(): 
+    print("say hey")
+print("top")
+sayhi()
+print("bottom")
---
+++ tuples.py
@@ -0,0 +1,12 @@
+#tuples are different than lists
+#most commonly for coordinates or something of this nature
+#tuples are immutable
+#once you establish varibles for tuples, they cannot be updated
+
+coordinates = (4, 5, 3)
+print(coordinates[2])
+
+#lists use square brackets - these are immutable 
+
+print(coordinates)
+
