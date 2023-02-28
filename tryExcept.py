6d9151009810c645d56873231ac51436aaca5029
Marty De Alba <103968668+martydealba27@users.noreply.github.com>
2023-02-28 09:35:06 -0800

    Add files via upload

---
+++ tryExcept.py
@@ -0,0 +1,9 @@
+try:
+    
+    number = float(input("Enter a number: "))
+    print(number)
+except: 
+    print("Invalid Input.")
+
+#try except block will allow your program to try out a piece of code
+#try except block won't break our code if a user enters a wrong value 
