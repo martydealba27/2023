f199487d16e1d3d4cbb497b9f636d9228a72292e
Marty De Alba <103968668+martydealba27@users.noreply.github.com>
2023-02-28 12:26:30 -0800

    Add files via upload

---
+++ modules.py
@@ -0,0 +1,18 @@
+#modules enable you to take functions from other python files
+
+#you can use python to make word documents thourgh modules
+
+#this is a also a file to make a class 
+
+
+class Student: 
+
+    def __init__(self, name, major, gpa):
+        self.name = name
+        self.major = major
+        self.gpa = gpa
+
+    
+student1 = Student("Marty", "Comms", 4.0,)
+
+print(student1.name) 
