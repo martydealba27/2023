975182aa17c409ca9f9f10c1846c7dc3a128ff60
Marty De Alba <103968668+martydealba27@users.noreply.github.com>
2023-02-28 09:25:02 -0800

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,13 @@
+print("Comments are fun")
+
+#anything after the pound sign gets ignored 
+
+#comments are plain text to help understand whats happening in a file 
+
+#This is a comment
+
+#Generally if you have comments for multuple lines, use multiple # 
+
+'''this is also a comment'''
+
+#comment out a line a of code 
